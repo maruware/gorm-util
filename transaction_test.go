683eed675840c2ba35829399ebc86c20aa0f3f9f
maruware <me@maruware.com>
2020-01-16 11:39:37 +0900

    pass test TransactTwo

--- transaction_test.go
+++ transaction_test.go
@@ -39,3 +39,44 @@ func TestTransaction(t *testing.T) {
 		t.Errorf("expect to regist user")
 	}
 }
+
+func TestTransactionTwo(t *testing.T) {
+	db, err := buildDatabase()
+	if err != nil {
+		t.Fatalf("failed to build database: %v", err)
+	}
+
+	user, post, err := util.TransactTwo(db, func(tx *gorm.DB) (interface{}, interface{}, error) {
+		user := User{Name: "takashi"}
+		if err := tx.Create(&user).Error; err != nil {
+			return nil, nil, err
+		}
+
+		post := Post{UserID: user.ID, Title: "My post"}
+		if err := tx.Create(&post).Error; err != nil {
+			return nil, nil, err
+		}
+
+		return user, post, nil
+	})
+
+	if err != nil {
+		t.Fatalf("failed transaction")
+	}
+
+	u, ok := user.(User)
+	if !ok {
+		t.Fatalf("failed type assertion")
+	}
+	if u.ID <= 0 {
+		t.Errorf("expect to regist user")
+	}
+
+	p, ok := post.(Post)
+	if !ok {
+		t.Fatalf("failed type assertion")
+	}
+	if p.ID <= 0 {
+		t.Errorf("expect to regist post")
+	}
+}
